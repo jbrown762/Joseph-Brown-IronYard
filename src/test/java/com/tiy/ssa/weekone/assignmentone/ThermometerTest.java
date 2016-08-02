0bbfd816319eba93b6e6305d3f8cfe54f2dcb4e1
Joe Brown <jbrown762@gmail.com>
2016-08-02 10:03:22 -0400

    updated test cases

--- src/test/java/com/tiy/ssa/weekone/assignmentone/ThermometerTest.java
+++ src/test/java/com/tiy/ssa/weekone/assignmentone/ThermometerTest.java
@@ -10,11 +10,24 @@ public class ThermometerTest
 	@Test
 	public void isEqualTest()
 	{
+		assertEquals("", 78, new Thermometer(78, true).display(true));
+		assertEquals("", 8, new Thermometer(8, false).display(false));
+	}
+
+	@Test
+	public void intConvertTest()
+	{
+		assertEquals("", 537, new Thermometer(999, true).display(false));
+		assertEquals("", 104, new Thermometer(40, false).display(true));
+		assertEquals("", 4, new Thermometer(40, true).display(false));		
+	}
+
+	@Test
+	public void decimalConvertTest()
+	{
 		assertEquals("", 26, new Thermometer(78.3, true).display(false));
 		assertEquals("", 174, new Thermometer(78.9, false).display(true));
-		assertEquals("", 537, new Thermometer(999, true).display(false));
 		assertEquals("", 213, new Thermometer(100.65, false).display(true));
-		assertEquals("", 78, new Thermometer(78, true).display(true));
 		assertEquals("", 100, new Thermometer(99.8, false).display(false));
 	}
 
